1dd6bb53bd3208a332b1998c36b756bec8948108
TomasSiso2000 <112671894+TomasSiso2000@users.noreply.github.com>
2025-01-20 14:08:51 -0300

    mas oscuro 44 scripts.js

--- scripts.js
+++ scripts.js
@@ -1,86 +1,72 @@
-let menu= document.getElementById("menu");
-
-let toggle_open= document.getElementById("toggle_open");
-let toggle_close= document.getElementById("toggle_close");
+let menu = document.getElementById("menu");
+let toggle_open = document.getElementById("toggle_open");
+let toggle_close = document.getElementById("toggle_close");
 
 toggle_open.addEventListener("click", toggleMenu);
 toggle_close.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
+function toggleMenu() {
     menu.classList.toggle("show-menu");
 
-    if (menu.classList.contains("show-menu")){
-        toggle_open.style.display="none";
-        toggle_close.style.display="block";
-    }
-    else{
-        toggle_open.style.display="block";
-        toggle_close.style.display="none";
+    if (menu.classList.contains("show-menu")) {
+        toggle_open.style.display = "none";
+        toggle_close.style.display = "block";
+    } else {
+        toggle_open.style.display = "block";
+        toggle_close.style.display = "none";
     }
 }
 
 /* --------------------VALIDAR FORMULARIO ---------------------------*/
+const formulario = document.getElementById("form-group");
+const inputs = document.querySelectorAll("#form-group input");
 
-const formulario= document.getElementById("form-group");
-const inputs= document.querySelectorAll("#form-group input");
+const expresiones = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+};
 
-const expresiones = {   /*------------------------ EXPRESIONES REGULARES --------------------------*/
-	name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-}
-
-const campos = {  /* LOS INICIALIZAMOS EN FALSE (ESTO PARA QUE NO SE PUEDA ENVIAR FORMULARIO CON ERRORES EN LOS CAMPOS) */
+const campos = {
     name: false,
     email: false,
-
-}
+};
 
 const validarFormulario = (e) => {
-    switch (e.target.name){
-        case "name":  /*VALIDAMOS EL CAMPO NOMBRE */
-            validarCampo(expresiones.name, e.target, "name"); /*EJECUTAMOS LA FUNCION VALIDAR CAMPO, PASANDOLE LA EXPRESION REGULAR PARA VALIDAR EL CAMPO(expresiones.name), LE PASAMOS EL INPUT(e.target) Y EL NOMBRE DEL CAMPO*/
-        break;
-
+    switch (e.target.name) {
+        case "name":
+            validarCampo(expresiones.name, e.target, "name");
+            break;
         case "email":
             validarCampo(expresiones.email, e.target, "email");
-        break;
+            break;
     }
 }
 
-const validarCampo = (expresion, input, campo) =>{
-    if(expresion.test(input.value)){  /*VALIDAMOS EL VALOR QUE INGRESAMOS EN EL CAMPO/INPUT */
-        document.getElementById(`grupo_${campo}`).classList.remove("form-group-incorrecto");  /* SACA EL COLOR ROJO */      /* (`grupo_${campo}`)  TOMA EL VALOR QUE LE PASEMOS A LA VARIABLE DESDE LOS "CASE" DE ARRIBA . ESTO LO USAMOS ASI PARA NO TENER QUE REPETIR LO MISMO CON EL CASE DE NOMBR Y EL DE EMAIL*/
-        document.getElementById(`grupo_${campo}`).classList.add("form-group-correcto");       /* PONE EL COLOR VERDE */
-        document.querySelector(`#grupo_${campo} i`).classList.add("fa-circle-check");          /*PONE EL ICONO DEL CHECK O TILDE*/
-        document.querySelector(`#grupo_${campo} i`).classList.remove("fa-circle-xmark");        /* SACA EL ICONO DE LA CRUZ*/
-        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo"); /*SACA EL MENSAJE DE ERROR*/
-        campos[campo] = true;  /*ESTO SERIA COMO PONER campos[email]=true, pero está de forma dinamica */
-
-
-
-    }
-    else{
-        document.getElementById(`grupo_${campo}`).classList.add("form-group-incorrecto");       /* PONE EL COLOR ROJO */
-        document.getElementById(`grupo_${campo}`).classList.remove("form-group-correcto");      /* SACA EL COLOR VERDE */
-        document.querySelector(`#grupo_${campo} i`).classList.add("fa-circle-xmark");           /*PONE EL ICONO DE LA CRUZ*/
-        document.querySelector(`#grupo_${campo} i`).classList.remove("fa-circle-check");           /* SACA EL ICONO DEL TILDE O CHECK*/
-        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");     /*PONE EL MENSAJE DE ERROR*/
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.getElementById(`grupo_${campo}`).classList.remove("form-group-incorrecto");
+        document.getElementById(`grupo_${campo}`).classList.add("form-group-correcto");
+        document.querySelector(`#grupo_${campo} i`).classList.add("fa-circle-check");
+        document.querySelector(`#grupo_${campo} i`).classList.remove("fa-circle-xmark");
+        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
+        campos[campo] = true;
+    } else {
+        document.getElementById(`grupo_${campo}`).classList.add("form-group-incorrecto");
+        document.getElementById(`grupo_${campo}`).classList.remove("form-group-correcto");
+        document.querySelector(`#grupo_${campo} i`).classList.add("fa-circle-xmark");
+        document.querySelector(`#grupo_${campo} i`).classList.remove("fa-circle-check");
+        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
         campos[campo] = false;
     }
 }
 
-inputs.forEach((input) => {       /* PARA CADA INPUT..... */
-    input.addEventListener("keyup", validarFormulario);   /* AL APRETAR UNA TECLA, QUE SE EJECUTE validarFormulario */
-    input.addEventListener("blur", validarFormulario);   /* AL HACER CLICK AFUERA DEL PUNTO , QUE SE EJECUTE validarFormulario */
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
 });
 
-formulario.addEventListener("submit", (e) => {    /* ESTO ESTÁ PREVINIENDO QUE SE ENVÍE EL FORMULARIO SI HAY ERRORES */
-    
-
-    if(campos.name==false || campos.email==false){ /* EN CASO DE QUE EL CAMPO DE NOMBRE O EMAIL ESTE VACIO, SE DETIENE EL COMPORTAMIENTO (O SEA, NO SE ENVIA MENSAJE) */
-        e.preventDefault();    /* CUANDO ENVIO MENSAJE, OCURRE UN TIMEOUT Y NO SE MANDA ??????????? */
-       
-
+formulario.addEventListener("submit", (e) => {
+    if (campos.name == false || campos.email == false) {
+        e.preventDefault();
     }
-    
 });
